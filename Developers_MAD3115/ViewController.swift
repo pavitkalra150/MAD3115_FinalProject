03eabccbaad7981ca81bba782a334a999f96b326
pavitkalra150 <c0868150@mylambton.ca>
2022-10-26 19:46:05 -0400

    Initial Setup and Register View Controller

--- Developers_MAD3115/ViewController.swift
+++ Developers_MAD3115/ViewController.swift
@@ -14,6 +14,14 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    
+    @IBAction func Addbutton(_ sender: UIButton) {
+        let mainSB : UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        let RegisterScene = mainSB.instantiateViewController(withIdentifier: "RegisterScene")
+        
+        navigationController?.pushViewController(RegisterScene, animated: true)
+    }
+    
 
 }
 
